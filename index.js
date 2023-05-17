3525cd80ab149082b7efd23c1974d701657d9848
dev-moshiur <dev.moshiurr@gmail.com>
2023-05-17 23:59:32 +0600

    commited

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(express.json());
 
 app.use(cors({
   origin:[
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://student-management-five-gold.vercel.app'
   ]
 }));
 
